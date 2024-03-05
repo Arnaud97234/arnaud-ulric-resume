ae824417bc5a6efc73702c9750ee85be513e6d26
Arnaud97234 <arnaud.ulric@gmail.com>
2024-03-05 17:34:41 +0100

    polish css + change fonts

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { StyledEngineProvider } from '@mui/material/styles'
+import '../styles/fonts/webfontkit-Abel/abel-regular-webfront.woff'
 
 config.autoAddCss = false
 
@@ -17,7 +19,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
+        <StyledEngineProvider injectFirst>
         <Component {...pageProps} />
+        </StyledEngineProvider>
       </Provider>
     </>
   )
