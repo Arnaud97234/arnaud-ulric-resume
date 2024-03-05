f3f1ddf399ebd57c33bb67d94939c976ce1121c5
Arnaud97234 <arnaud.ulric@gmail.com>
2024-03-05 21:39:25 +0100

    fix cors policy error

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  });
+app.use((res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
 
 app.use('/', indexRouter)
 
