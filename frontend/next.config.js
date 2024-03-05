889cbbee29808e5abf42b5a79acecd7919315256
Arnaud97234 <arnaud.ulric@gmail.com>
2024-03-05 19:21:22 +0100

    fix css issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
               source: "/api/special-data",
               headers: [
                   { key: "Access-Control-Allow-Credentials", value: "false" },
-                  { key: "Access-Control-Allow-Origin", value: "https://example.com" },
+                  { key: "Access-Control-Allow-Origin", value: "*" },
                   { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                   { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
               ]
