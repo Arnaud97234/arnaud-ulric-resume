18847fa18b75ae14cc75d18d4d070b3d94a5b6a3
Arnaud97234 <arnaud.ulric@gmail.com>
2024-03-05 19:29:22 +0100

    fix css issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,31 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
 const nextConfig = {
-  async headers() {
-      return [
-          {
-              // matching all API routes
-              source: "/api/:path*",
-              headers: [
-                 // omitted for brevity...
-              ]
-          },
-          {
-              source: "/api/special-data",
-              headers: [
-                  { key: "Access-Control-Allow-Credentials", value: "false" },
-                  { key: "Access-Control-Allow-Origin", value: "*" },
-                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                  { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-              ]
-          }
-      ]
-  }
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
