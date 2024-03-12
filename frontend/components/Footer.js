b6e64b49be149f5003e0a8a0a812233bbf32687d
Arnaud97234 <arnaud.ulric@gmail.com>
2024-03-12 02:54:33 +0100

    footer + homepage polish

---
+++ frontend/components/Footer.js
@@ -0,0 +1,18 @@
+import styles from "../styles/Footer.module.css"
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIdBadge } from '@fortawesome/fontawesome-free-solid'
+
+import { StyledEngineProvider } from '@mui/material/styles'
+
+function Footer() {
+
+    return (
+        <div className={styles.footer}>
+            <span className={styles.icon}><FontAwesomeIcon icon={faIdBadge} /></span>
+            <span className={styles.signature}>Arnaud97234 - 2024</span>
+        </div>
+    )
+}
+
+export default Footer
