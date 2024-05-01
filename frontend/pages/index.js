5f71e1e99a258f897470ecaba42ad90adbf8f9d1
Arnaud97234 <arnaud.ulric@gmail.com>
2024-05-01 22:48:26 +0200

    nextJS: add head title to pages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,12 @@
 import Home from '../components/Home'
+import Head from 'next/head'
 
 function Index() {
 	return (
 		<>
+			<Head>
+				<title>Profile</title>
+			</Head>
 			<Home />
 		</>
 	)
